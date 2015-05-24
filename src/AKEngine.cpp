adddf11f9bb99e3ec885be4e9820eea12a13b993
gbytz <gbytz.zyt@gmail.com>
2015-05-23 21:52:58 -0300

    Unused subsystem disabled

--- src/AKEngine.cpp
+++ src/AKEngine.cpp
@@ -62,12 +62,12 @@ bool AKEngine::start()
                 success = false;
             }
 
-            //Initialize SDL_mixer
-            if( Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 ) < 0 )
-            {
-                printf( "SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError() );
-                success = false;
-            }
+            // //Initialize SDL_mixer
+            // if( Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 ) < 0 )
+            // {
+            //     printf( "SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError() );
+            //     success = false;
+            // }
         }
     }
 
@@ -101,7 +101,7 @@ void AKEngine::stop()
     gRenderer = NULL;
 */
     //Quit SDL subsystems
-    Mix_Quit();
+    // Mix_Quit();
     TTF_Quit();
     IMG_Quit();
     SDL_Quit();
